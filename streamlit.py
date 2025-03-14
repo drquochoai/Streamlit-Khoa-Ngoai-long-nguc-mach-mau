1709812ea6815c736b490d973bd38051ee78324e
Tran Quoc Hoai <bsquochoainew@gmail.com>
2025-03-14 14:36:52 +0700

    Remove Dev Container configuration and clean up Streamlit app code

--- streamlit.py
+++ streamlit.py
@@ -3,10 +3,3 @@ import numpy as np
 
 # add a title
 st.title('My First Streamlit App')
-
-# navigate to a _1_Trang_Chủ.py page
-# Navigate to _1_Trang_Chủ.py
-if st.button("Go to Home Page"):
-    # Requires Streamlit 1.16.0 or newer
-    st.switch_page("pages/Trang_Chu.py")
-# add a file input to the sidebar
